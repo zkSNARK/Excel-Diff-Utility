e71dd86f616afa507aceab601df1fe524ae8e737
zkSNARK <zkSNARK@users.noreply.github.com>
2019-06-27 23:06:57 -0400

    Update excell_test.py

--- excell_test.py
+++ excell_test.py
@@ -1,10 +1,34 @@
+"""A utility to diff 2 Excel files and report the cells which have changed.
+
+Expects an Excel file to be under version control in a local repo.
+
+Does not handle credential input or other git configuration elements. If
+the git commands
+"""
+
+import argparse
 import os
 import datetime
-
-from openpyxl import load_workbook
 import subprocess
 import shutil
+import git
+
 import pandas as pd
+import numpy as np
+
+from xlsxwriter.utility import xl_rowcol_to_cell
+
+
+def is_git_directory(path='.'):
+    return subprocess.call(['git', '-C', path, 'status'], stderr=subprocess.STDOUT, stdout=open(os.devnull, 'w')) == 0
+
+
+def handle_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-g", "--make_git", help="create local git repo if none exists", action="store_true")
+    parser.add_argument("-f", "--file", help="The file we are tracking.", action="store_true")
+    args = parser.parse_args()
+    return args.make_git, args.file
 
 
 def tmp_name(excel_file):
@@ -17,27 +41,59 @@ def rename_file(excel_file):
     output = process.communicate()[0]
 
 
-def load_excel_wb(excel_file):
-    wb2 = load_workbook('test.xlsx')
-    print(wb2.sheetnames)
-    return wb2
-
-
 def copy_file_to_tmp(excel_file):
     shutil.copyfile(excel_file, tmp_name(excel_file))
 
 
-def reset_file_in_git(excel_file):
-    process = subprocess.Popen(["git", "checkout", excel_file], stdout=subprocess.PIPE)
+def reset_file_in_git(commit_id: str, excel_file: str):
+    process = subprocess.Popen(["git", "checkout", commit_id, excel_file], stdout=subprocess.PIPE)
     output = process.communicate()[0]
 
 
-def diff_old_and_new_file(excel_file):
-    df1 = pd.read_excel(excel_file)
-    df2 = pd.read_excel(tmp_name(excel_file))
-    difference = df1[df1 != df2]
-    print(difference)
-    return difference
+def diff_old_and_new_file(file1, file2):
+    """Compare 2 excel files and report differences.
+
+    Uses the general idea from https://stackoverflow.com/a/52763561/6840486
+    and adds handling for Excel work sheets.
+    """
+
+    xlsx_template = pd.ExcelFile(file1)
+    xlsx_test = pd.ExcelFile(file2)
+
+    for sheet in xlsx_template.sheet_names:
+        template = pd.read_excel(xlsx_template, sheet, na_values=np.nan, header=None)
+        test_sheet = pd.read_excel(xlsx_test, sheet, na_values=np.nan, header=None)
+
+        rt, ct = template.shape  # row, col of template
+        rtest, ctest = test_sheet.shape  # row, col of test
+
+        df = pd.DataFrame(columns=['Cell_Location', 'Previous_Value', 'Current_Value'])
+
+        for rowNo in range(max(rt, rtest)):
+            for colNo in range(max(ct, ctest)):
+                # Fetching the template value at a cell
+                try:
+                    template_val = template.iloc[rowNo, colNo]
+                except:
+                    template_val = np.nan
+
+                # Fetching the testsheet value at a cell
+                try:
+                    test_sheet_val = test_sheet.iloc[rowNo, colNo]
+                except:
+                    test_sheet_val = np.nan
+
+                # Comparing the values
+                if str(template_val) != str(test_sheet_val):
+                    cell = xl_rowcol_to_cell(rowNo, colNo)
+                    dfTemp = pd.DataFrame([[cell, template_val, test_sheet_val]],
+                                          columns=['Cell_Location', 'Previous_Value', 'Current_Value'])
+                    df = df.append(dfTemp)
+
+        if not df.index.empty:
+            print("CHANGES DETECTED IN SHEET : " + sheet)
+            print(df)
+            print()
 
 
 def copy_tmp_back(excel_file):
@@ -51,29 +107,86 @@ def delete_file(file):
 def commit_changes(excel_file):
     process = subprocess.Popen(["git", "add", excel_file], stdout=subprocess.PIPE)
     output = process.communicate()[0]
-    process = subprocess.Popen(["git", "commit", "-m", f"File updated {datetime.datetime.now()}"],
+    process = subprocess.Popen(["git", "commit", "-m", f"File auto-updated {datetime.datetime.now()}"],
                                stdout=subprocess.PIPE)
+    output = process.communicate()[0]
+
+
+def changes_detected(file):
+    repo = git.Repo(".")
+    changed = [item.a_path for item in repo.index.diff(None)]
+    return file in changed
+
+
+def changes_detected_from_commit(file, commit):
+    changed = [item.a_path for item in commit.diff(None)]
+    return file in changed
+
+
+def create_repo_here():
+    repo = git.Repo.init(".")
+    return repo
+
+
+def select_commit_for_diff(repo: git.Repo):
+    l = []
+    print()
+    print("Select from available checkpoints.")
+    print("----------------------------------")
+    for i, c in enumerate(repo.iter_commits()):
+        print(f"  [{i}] Save point : {c}: {c.authored_datetime}")
+        l.append(c)
+
+    print()
+    user_select = int(input("Which checkpoint would you like to diff against (enter for most recent)? "))
+    return l[user_select]
 
 
 def main():
-    excel_file = "test.xlsx"
+    repo = None
 
-    load_excel_wb(excel_file)
-    copy_file_to_tmp(excel_file)
-    reset_file_in_git(excel_file)
+    make_git, file = handle_args()
+    if make_git:
+        if is_git_directory():
+            repo = git.Repo(".")
+            print("Git repo creation requested through flags, but repo already exists in current directory.")
+        else:
+            create_repo_here()
+            if file:
+                repo = git.Repo(".")
+                repo.index.add([file])
+                repo.index.commit("initial commit")
+                print("Git repo created in current directory and added file: " + file + ".")
+            else:
+                print("Created git repo in current directory.")
+    else:
+        repo = git.Repo(".")
 
-    difference = diff_old_and_new_file(excel_file)
+    if file:
+        print("requested to run on file : " + file)
+    else:
+        print("Using default file : test.xlsx")
+        excel_file = "test.xlsx"
 
-    copy_tmp_back(excel_file)
-    delete_file(tmp_name(excel_file))
-    commit_changes(excel_file)
+    while True:
+        commit = select_commit_for_diff(repo)
+        changed = changes_detected_from_commit(excel_file, commit)
+
+        if changed:
+            print("Changes detected.")
+            copy_file_to_tmp(excel_file)
+
+            # select commit for diff and reset file in git to that commit
+            reset_file_in_git(str(commit), excel_file)
+
+            diff_old_and_new_file(excel_file, tmp_name(excel_file))
+
+            copy_tmp_back(excel_file)
+            delete_file(tmp_name(excel_file))
+            commit_changes(excel_file)
+        else:
+            print("no changes detected")
 
 
 if __name__ == "__main__":
-    # df1 = pd.read_excel("test.xlsx")
-    # df2 = pd.read_excel("test2.xlsx")
-    # difference = df1[df1 != df2]
-    # print(difference)
-
-    # excel_file = "test.xlsx"
     main()
